a2779935414662e8060406ab6ec6b8411144e91c
Masood Usmani <masood.usmani@freighthub.com>
2020-11-01 13:19:47 +0100

    add post

--- web/src/components/BlogPostsCell/BlogPostsCell.js
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -1,5 +1,4 @@
-import { Link, routes } from '@redwoodjs/router'
-
+import BlogPost from 'src/components/BlogPost'
 export const QUERY = gql`
   query BlogPostsQuery {
     posts {
@@ -18,15 +17,5 @@ export const Empty = () => <div>Empty</div>
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ posts }) => {
-  return posts.map((post) => (
-    <article key={post.id}>
-      <header>
-        <h2>
-          <Link to={routes.blogPost({ id: post.id })}>{post.title}</Link>
-        </h2>
-      </header>
-      <p>{post.body}</p>
-      <div>Posted at: {post.createdAt}</div>
-    </article>
-  ))
+  return posts.map((post) => <BlogPost post={post} key={post.id} />)
 }
